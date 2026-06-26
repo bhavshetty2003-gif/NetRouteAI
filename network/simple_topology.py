c3e055fb96b8d16d5ed02804b78b8e3742d5e539
bhavshetty2003-gif <bhavshetty2003@gmail.com>
2026-06-26 15:21:04 +0530

    Day 2: Created first Mininet topology

--- network/simple_topology.py
+++ network/simple_topology.py
@@ -1,12 +1,12 @@
 from mininet.net import Mininet
 from mininet.topo import Topo
-from mininet.node import Controller
+from mininet.node import OVSKernelSwitch
 from mininet.cli import CLI
 
 class SimpleTopo(Topo):
     def build(self):
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
 
         s1 = self.addSwitch('s1')
 
@@ -17,7 +17,8 @@ topo = SimpleTopo()
 
 net = Mininet(
     topo=topo,
-    controller=Controller
+    switch=OVSKernelSwitch,
+    controller=None
 )
 
 net.start()
