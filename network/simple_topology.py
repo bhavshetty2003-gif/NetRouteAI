9ed7f83dba6187205d626b57811ad90709a5b7ab
bhavshetty2003-gif <bhavshetty2003@gmail.com>
2026-06-26 13:26:21 +0530

    Add project folder structure

---
+++ network/simple_topology.py
@@ -0,0 +1,29 @@
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.node import Controller
+from mininet.cli import CLI
+
+class SimpleTopo(Topo):
+    def build(self):
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+
+        s1 = self.addSwitch('s1')
+
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+
+topo = SimpleTopo()
+
+net = Mininet(
+    topo=topo,
+    controller=Controller
+)
+
+net.start()
+
+print("Network Started Successfully!")
+
+CLI(net)
+
+net.stop()
